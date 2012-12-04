443b39ff4d61fdf9c838eb8a926f256f119dac45
Vasily Medyanikov <aegnor.evernight@gmail.com>
2012-12-05 02:16:54 +0400

    it works?..

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ def logistic_function_derivative(x):
 class MultilayerNetwork:
 
     def _init_weights(self, n, m):
-        return npr.rand(n, m).transpose()
+        return npr.rand(n, m).transpose() * 0.4
 
     def __init__(self, network_size):
         """
@@ -26,7 +26,7 @@ class MultilayerNetwork:
 
         self.network_size = network_size
 
-    def process_learning(self, sample, output):
+    def process_learning(self, sample, result):
         """
         Back-propagation algorithm
         """
@@ -38,42 +38,68 @@ class MultilayerNetwork:
             1,
             sample.transpose()
         ))
+        output = np.vstack((
+            1,
+            result.transpose()
+        ))
+
         v = {0: input}
         y = {0: input}
         for l in xrange(1, len(self.network_size)):
-            v[l] = W[l - 1].dot(y[l - 1])
-            y[l] = np.vstack((
+            v[l] = np.vstack((
                 1,
-                logistic_function(v[l])
+                W[l - 1].dot(y[l - 1])
             ))
+            y[l] = logistic_function(v[l])
 
         # backward pass
         error = {}
         delta = {}
         for l in reversed(xrange(len(self.network_size))):
             if l == len(self.network_size) - 1:
-                error[l] = output.transpose() - y[l]
+                error[l] = output - y[l]
             else:
-                error[l] = W[l].transpose().dot(delta[l + 1])
+                WT = W[l].transpose()
+                M = np.hstack((np.zeros((WT.shape[0], 1)), WT))
+                error[l] = M.dot(delta[l + 1])
             delta[l] = np.array(error[l]) * logistic_function_derivative(np.array(v[l]))
 
         # modify weights
         for l in reversed(xrange(1, len(self.network_size))):
-            W[l - 1] += learning_rate * delta[l].dot(y[l - 1].transpose())
+            deltas_except_bias = np.vsplit(delta[l], (1,))[1]
+            W[l - 1] += learning_rate * deltas_except_bias.dot(y[l - 1].transpose())
 
     def train_at_set(self, training_set):
         samples, values = np.hsplit(training_set, (-m,))
         for sample, value in zip(samples, values):
             self.process_learning(sample, value)
 
+    def error_at_set(self, test_set):
+        E = 0
+
+        samples, values = np.hsplit(test_set, (-m,))
+        for sample, value in zip(samples, values):
+            res = self.process(sample)
+            print res, value
+            E += np.square(res - value).sum()
+
+        return E/2
+
     def process(self, sample):
         """
         Straightforward computation
         """
-        cur = sample.transpose()
+        input = np.vstack((
+            1,
+            sample.transpose()
+            ))
+        cur = input
         for l in xrange(len(self.network_size) - 1):
-            cur = logistic_function(self.weights[l].dot(cur))
-        return cur
+            cur = np.vstack((
+                1,
+                logistic_function(self.weights[l].dot(cur))
+            ))
+        return np.vsplit(cur, ())
 
 def load_samples(fname):
     with open(fname, 'r') as f:
@@ -88,8 +114,11 @@ if __name__ == '__main__':
     n, m, data = load_samples('../data/circles.tsv')
     net = MultilayerNetwork((2, 10, 1))
 
-    net.train_at_set(data)
+    print net.error_at_set(data)
+    for i in xrange(10):
+        net.train_at_set(data)
+        print net.error_at_set(data)
 
-    samples, values = np.hsplit(data, (-m,))
-    for sample, value in zip(samples, values):
-        print net.process(sample), value
+#    samples, values = np.hsplit(data, (-m,))
+#    for sample, value in zip(samples, values):
+#        print net.process(sample), value
